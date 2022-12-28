23df41fe829fd435b80dae8cb3e89eeab21b62dd
Jessie  Detchou <jessie.detchou@gmail.com>
2022-12-27 19:11:11 -0500

    Update constituents_store.py

--- indexMaker/constituents_store.py
+++ indexMaker/constituents_store.py
@@ -44,215 +44,215 @@ CULTURE_INDEX_CONSTITUENTS = [
 JORDAN_INDEX_CONSTITUENTS = [
   {
     "sku" : "555088-117",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "136064-123",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "378037-003",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "308497-100",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "845035-003",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "CD4487-100",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
    {
     "sku" : "378037-061",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "AV9175-001",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "378037-117",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "AA3834-101",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   }
 ]
 
 NIKE_INDEX_CONSTITUENTS = [
   {
     "sku" : "315121-115/CW2290-111",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "BQ6817-006",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "AA3830-002",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "318429-660",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "314996-001",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "AH8050-100",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
    {
     "sku" : "884421-001",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "616750-001",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "CU3244-100",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "DD1391-100",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   }
 ]
 
 YEEZY_INDEX_CONSTITUENTS = [
   {
     "sku" : "CP9654",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "GW1934",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "B75571",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "HP8739",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "EG6462",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "FW5191",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
    {
     "sku" : "DB2908",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "EF2829",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "GX6144",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "H03665",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   }
 ]
 
 ADIDAS_INDEX_CONSTITUENTS = [
   {
     "sku" : "GZ9256",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "BB7802",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "GY9693",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "GZ9177",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "H01877",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "M20324",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
    {
     "sku" : "EE7582",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "FX3239",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "HP6772",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "FY9120",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   }
 ]
 
 NEW_BALANCE_INDEX_CONSTITUENTS = [
   {
     "sku" : "BB550PB/BB550PB1",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "ML574EGG",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "M990JD3",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "MR993ALD",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "CM997HZH",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "U9060JF1",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
    {
     "sku" : "MR993GL", #warning return Kirth Show
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   },
     {
     "sku" : "M990SB2",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "M990GL5",
-    "weight_in_decimal" : 0.02
+    "weight_in_decimal" : 0.1
   },
    {
     "sku" : "MS997JBG",
-     "weight_in_decimal" : 0.02
+     "weight_in_decimal" : 0.1
   }
 ]
 
