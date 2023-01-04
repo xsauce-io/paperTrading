f4fc6ad95ccb393a499d096e0e89577621d9d21f
Jessie  Detchou <jessie.detchou@gmail.com>
2023-01-04 11:50:16 -0500

    clean up controller

--- processes/close.py
+++ processes/close.py
@@ -27,7 +27,7 @@ def close(sender, message):
     current_participant_info = controller.get_participant_info(sender)
 
     updated_position, updated_participant, new_trade = determine_closed_position_update(reduction, direction, current_position_info, current_participant_info, current_index)
-    updated_trades = controller.append_trade_to_participant_trades(sender, new_trade)
+    updated_trades = controller.add_trade_to_participant_trades(sender, new_trade)
     controller.update_participant_position(sender, index_name, updated_position, updated_participant, updated_trades)
 
     return '{} position has been closed!'.format(new_trade.direction)
--- processes/open.py
+++ processes/open.py
@@ -27,7 +27,7 @@ def open(sender, message):
     current_participant_info = controller.get_participant_info(sender)
 
     updated_position, updated_participant, new_trade = determine_opened_position_update(wager, direction, current_position_info, current_participant_info, current_index)
-    updated_trades = controller.append_trade_to_participant_trades(sender, new_trade)
+    updated_trades = controller.add_trade_to_participant_trades(sender, new_trade)
     controller.update_participant_position(sender, index_name ,updated_position, updated_participant, updated_trades)
 
     return '{} position has been opened!'.format(new_trade.direction)
