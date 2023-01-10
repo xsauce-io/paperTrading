0fe238290de230485833502c3f5a177d6c16bb6b
Jessie  Detchou <jessie.detchou@gmail.com>
2023-01-10 14:08:07 -0500

    update play name

--- processes/play.py
+++ processes/play.py
@@ -3,19 +3,26 @@ from models import *
 
 
 #public
-def play(sender):
+def play(sender, id):
+
+    if (sender == None):
+        sender = str(id)
     if find_participant(sender):
         return("Nice try! No such thing as free")
     else:
-        participant = create_participant(sender)
+
+        participant = create_participant(sender, id)
         return("Welcome {},\nThis is v0 of Daily Bread. Your account has been funded with $10,000".format(participant.name))
 
 
 def find_participant(sender):
     return controller.find_participant(sender)
 
-def create_participant(sender):
+def create_participant(sender,id):
+    print(id)
     init_funds = 10000
+    if (sender == None):
+        sender = str(id)
     participant = Participant(sender, init_funds, number_of_trades=0)
     controller.add_participant(participant.name, participant.funds)
     return participant
